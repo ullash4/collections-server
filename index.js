49900fe12030ec9d12150ceee3684a98b407fd73
ullash4 <programmerullash@gmail.com>
2022-06-08 12:36:28 +0600

    all done

--- index.js
+++ index.js
@@ -17,10 +17,11 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
       await client.connect();
+      const usersCollection = client.db("DeskalaTask").collection("users")
       const candidatesCollection = client.db("DeskalaTask").collection("candidates")
 
-      // store candidates in database and issue jwt token
-      app.put('/candidates/:email', async(req, res)=>{
+      // store users in database
+      app.put('/users/:email', async(req, res)=>{
         const email = req.params.email;
         const user = req.body;
         const filter = {email : email};
@@ -28,9 +29,16 @@ async function run() {
         const updateDoc = {
           $set: user,
         }
-        const candidates = await candidatesCollection.updateOne(filter,updateDoc,options)
+        const candidates = await usersCollection.updateOne(filter,updateDoc,options)
         
         res.send(candidates)
+      })
+
+      // Create candidates
+      app.post('/candidates', async(req, res)=>{
+        const data = req.body;
+        const result = await candidatesCollection.insertOne(data)
+        res.send(result);
       })
 
       // get all candidates
@@ -46,6 +54,27 @@ async function run() {
         const query = {_id:ObjectId(id)}
         const result = await candidatesCollection.deleteOne(query);
         res.send(result)
+    })
+
+    // get single candidate
+    app.get('/candidate/:id', async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id:ObjectId(id)};
+      const result = await candidatesCollection.findOne(query)
+      res.send(result);
+    })
+
+    // Update candidates
+    app.put('/candidate/:id', async(req, res)=>{
+      const id = req.params.id;
+      const candidate = req.body;
+      const filter = {_id:ObjectId(id)};
+      const updateDoc = {
+        $set: candidate,
+      }
+      const candidates = await candidatesCollection.updateOne(filter,updateDoc)
+      
+      res.send(candidates)
     })
 
 
